ca3887eb26841dd819667bb1782bfb96def04e77
Steffi Land <git01@steffiland.de>
2020-10-29 22:27:22 +0100

    initial working setup

---
+++ README.md
@@ -0,0 +1,1 @@
+das hier: https://www.docker.com/blog/containerized-python-development-part-3/
---
+++ app/src/server.py
@@ -0,0 +1,16 @@
+# https://www.docker.com/blog/containerized-python-development-part-1/
+
+# debugger
+import ptvsd
+ptvsd.enable_attach(address=('0.0.0.0', 5678))
+
+
+from flask import Flask
+server = Flask(__name__)
+
+@server.route("/")
+ def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+   server.run(host='0.0.0.0')
