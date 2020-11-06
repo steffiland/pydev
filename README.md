7eadd4ff694133027e3a54269577d985be3df14c
Steffi Land <git01@steffiland.de>
2020-11-06 10:34:04 +0100

    working minimal example

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 das hier: https://www.docker.com/blog/containerized-python-development-part-3/
+
+Für Produktion pylint und ptvsd aus der requirements.txt entfernen
+
+TODO: run as nonroot
+* https://medium.com/@DahlitzF/run-python-applications-as-non-root-user-in-docker-containers-by-example-cba46a0ff384
--- app/src/server.py
+++ app/src/server.py
@@ -1,16 +1,16 @@
 # https://www.docker.com/blog/containerized-python-development-part-1/
 
 # debugger
-import ptvsd
-ptvsd.enable_attach(address=('0.0.0.0', 5678))
+#import ptvsd
+#ptvsd.enable_attach(address=('0.0.0.0', 5678))
 
 
 from flask import Flask
 server = Flask(__name__)
 
 @server.route("/")
- def hello():
-    return "Hello World!"
+def hello():
+    return "Hello World, edit and refresh rules!"
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
